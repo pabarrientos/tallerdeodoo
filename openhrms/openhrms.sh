eec9cff06dc08c7daa406358e14ffcf1f28b6ed5
Pablo Alberto Barrientos <pabarrientos@hotmail.com>
2019-07-01 20:12:18 -0300

    Create openhrms.sh

---
+++ openhrms/openhrms.sh
@@ -0,0 +1,4 @@
+sudo git clone https://github.com/CybroOdoo/OpenHRMS.git --depth 1 --branch 11.0 --single-branch /home/odoo/odoo/11/sources/openhrms
+pip3 install pandas
+sudo apt-get install -y nodejs npm
+sudo npm install -g less less-plugin-clean-css
